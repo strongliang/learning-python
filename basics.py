0ccf5711f12303fccca59db5c90792215ebcd1f0
Strong Liang <lysisius@gmail.com>
2012-02-25 16:44:51 -0800

    python test

---
+++ basics.py
@@ -0,0 +1,14 @@
+a, b = 1, 2 #assign two variables with one =
+print a, b
+
+print (1==2) == False # False is built in, and == is interchangeable with is here
+
+print 1 is 1
+
+print "abc" is "abc"
+print "abc" == "abc"
+
+print int("123")
+x = raw_input("hello\n")
+
+print "here's what you've entered", x
